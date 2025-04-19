e370a14634b8a43f6892294aedc89c79509e3c47
yujinashrestha <yujinashresths@gmail.com>
2025-04-19 18:35:14 +0545

    completed

--- Todolists/src/App.jsx
+++ Todolists/src/App.jsx
@@ -1,6 +1,6 @@
 import { use, useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TodoForm from './components/TodoForm.jsx'
+import TodoItem from './components/TodoItem.jsx'
 import './App.css'
 
 import { TodoProvider } from './contexts/Index.js'
@@ -19,7 +19,7 @@ function App() {
   }
 
   const toggleTodo=(id)=>{
-    setTodos((prev)=> prev.map((prevTodo)=>prevTodo.id===id? {...prevTodo, commpleted: !prevTodo.completed} : prevTodo))
+    setTodos((prev)=> prev.map((prevTodo)=>prevTodo.id===id? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
 
   }
 
@@ -31,18 +31,30 @@ function App() {
     }
   }, [])
 
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+
+  }, [todos])
+
   
 
   return (
-    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleTod0 }}>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleTodo }}>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id} className='w-full'>
+                            <TodoItem todo={todo} />
+
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
